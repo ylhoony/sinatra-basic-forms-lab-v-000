c6de4716fbabf335f051ad88d8340313e9a3efb7
Hoon Lee <yl.hoony@gmail.com>
2018-01-05 21:49:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    @result = Puppy.new(params[:name], params[:breed], params[:age])
+
+    erb :display_puppy
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
 end
